d63abd522e3dc31315f5a5d26d1cc0d5c90544ac
LeanVel <velasco.l.n@gmail.com>
2017-03-26 16:34:18 +0200

    Add files via upload

--- TasksParser.py
+++ TasksParser.py
@@ -7,36 +7,39 @@ def parseTasks(tasksFile, begintimeframe = 0, endtimeframe = int(time.time())) :
 
     listOfTasks = []
 
-    for tasks in tasksList["items"]:
+    try :
+        for tasks in tasksList["items"]:
 
-        taskListTitle= tasks["title"]
-        timeStamp = tasks["updated"]
+            taskListTitle= tasks["title"]
+            timeStamp = tasks["updated"]
 
-        timeStamp = (int(time.mktime(time.strptime(timeStamp, '%Y-%m-%dT%H:%M:%S.000Z'))) - time.timezone)
+            timeStamp = (int(time.mktime(time.strptime(timeStamp, '%Y-%m-%dT%H:%M:%S.000Z'))) - time.timezone)
 
-        # time filtering
-        if timeStamp < endtimeframe and timeStamp > begintimeframe:
+            # time filtering
+            if timeStamp < endtimeframe and timeStamp > begintimeframe:
 
-            for task in tasks["items"]:
+                for task in tasks["items"]:
 
-                timeStamp = task["updated"]
-                timeStamp = (int(time.mktime(time.strptime(timeStamp, '%Y-%m-%dT%H:%M:%S.000Z'))) - time.timezone)
-                title = task["title"]
-                try :
-                    notes = task["notes"]
-                except :
-                    notes = ""
-                try:
-                    dueDate = task["due"]
-                except:
-                    dueDate = ""
+                    timeStamp = task["updated"]
+                    timeStamp = (int(time.mktime(time.strptime(timeStamp, '%Y-%m-%dT%H:%M:%S.000Z'))) - time.timezone)
+                    title = task["title"]
+                    try :
+                        notes = task["notes"]
+                    except :
+                        notes = ""
+                    try:
+                        dueDate = task["due"]
+                    except:
+                        dueDate = ""
 
-                taskInfo = timeStamp, 'Tasks', \
-                    "Tasks List Title: " + taskListTitle + ", " \
-                    "Task Title: " + title + ", " \
-                    "Task notes: " + notes + ", " \
-                    "Due date: " + dueDate
+                    taskInfo = timeStamp, 'Tasks', \
+                        "Tasks List Title: " + taskListTitle + ", " \
+                        "Task Title: " + title + ", " \
+                        "Task notes: " + notes + ", " \
+                        "Due date: " + dueDate
 
-                listOfTasks.append(taskInfo)
+                    listOfTasks.append(taskInfo)
+    except :
+        print ("empty tasks")
 
     return listOfTasks
